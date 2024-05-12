4e1a8e976193cea65da2371f8448484e9b88c13e
patrickYoo <yhy1045@naver.com>
2024-05-13 00:12:56 +0900

    feat: 허트비트 추가

--- netty-client/src/main/java/com/patrick/nettyclient/channel/initialize/JmsClientChannelInitializer.java
+++ netty-client/src/main/java/com/patrick/nettyclient/channel/initialize/JmsClientChannelInitializer.java
@@ -2,9 +2,12 @@ package com.patrick.nettyclient.channel.initialize;
 
 import com.patrick.tcpprotocol.decode.ByteToPacketDecoder;
 import com.patrick.tcpprotocol.encode.SampleStringToByteEncoder;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+import java.util.concurrent.TimeUnit;
 import org.jetbrains.annotations.NotNull;
 
 public class JmsClientChannelInitializer extends ChannelInitializer<SocketChannel> {
@@ -18,5 +21,9 @@ public class JmsClientChannelInitializer extends ChannelInitializer<SocketChanne
 
         // encode
         pipeline.addLast(new SampleStringToByteEncoder());
+
+        // heartBeat trigger
+        pipeline.addLast(new IdleStateHandler(0, 3 * 1000, 0, TimeUnit.MILLISECONDS));
+        pipeline.addLast(new ChannelInboundHandlerAdapter());
     }
 }
